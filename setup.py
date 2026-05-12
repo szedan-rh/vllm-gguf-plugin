428984e6e2714b891477cf04ca2134a886f685fa
Isotr0py <mozf@mail2.sysu.edu.cn>
2026-05-13 01:06:11 +0800

    oops
    Signed-off-by: Isotr0py <mozf@mail2.sysu.edu.cn>

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@
 import sys
 
 from setuptools import setup
-<<<<<<< release-ci
 
 
 def _should_build_extension() -> bool:
@@ -38,27 +37,3 @@ if _should_build_extension():
     )
 
 setup(**setup_kwargs)
-=======
-from torch.utils.cpp_extension import BuildExtension, CUDAExtension
-
-setup(
-    ext_modules=[
-        CUDAExtension(
-            name="vllm_gguf_plugin._C_gguf",
-            sources=[
-                "vllm_gguf_plugin/csrc/torch_bindings.cpp",
-                "vllm_gguf_plugin/csrc/gguf/gguf_kernel.cu",
-            ],
-            include_dirs=[
-                "vllm_gguf_plugin/csrc",
-                "vllm_gguf_plugin/csrc/gguf",
-            ],
-            extra_compile_args={
-                "cxx": ["-O3", "-std=c++17"],
-                "nvcc": ["-O3", "-std=c++17", "--use_fast_math"],
-            },
-        )
-    ],
-    cmdclass={"build_ext": BuildExtension},
-)
->>>>>>> main
